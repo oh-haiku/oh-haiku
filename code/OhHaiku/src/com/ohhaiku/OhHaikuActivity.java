4a091c2145a3a1bef61655b8e80d7ba2047322e9
Jesper Josefsson <jesper.josefsson@gmail.com>
2012-04-03 16:11:08 +0200

    Add boilerplate for Eclipse - also move gitignore to Eclipse folder

---
+++ code/OhHaiku/src/com/ohhaiku/OhHaikuActivity.java
@@ -0,0 +1,13 @@
+package com.ohhaiku;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OhHaikuActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
