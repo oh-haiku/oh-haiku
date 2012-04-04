c244a89c2015177634396dc568911e15600a6e97
Jesper Josefsson <jesper.josefsson@gmail.com>
2012-04-04 11:13:14 +0200

    One more try with indentation

--- code/OhHaiku/src/com/ohhaiku/utility/NaiveSyllableCounter.java
+++ code/OhHaiku/src/com/ohhaiku/utility/NaiveSyllableCounter.java
@@ -7,23 +7,23 @@ public class NaiveSyllableCounter implements ISyllableCounter {
 
   public NaiveSyllableCounter() {}
   /*
-   * A regular expression that matches any number of consecutive vowels
-   */
-	private static Pattern vowels = Pattern.compile("([aouåeiyäö]+)*", Pattern.CASE_INSENSITIVE);
-	
-	/**
-	 * @return the number of syllables in the word
-	 */
-	public int count(String phrase) {
-	  Matcher vowelMatcher = vowels.matcher(phrase);
-	  int matches = 0;
-	  while (vowelMatcher.find()) {
-	    String match = vowelMatcher.group();
-	    if (!(match.length() == 0)) {
-	      System.out.println("Match: " + match);
-	      matches++;
-	    }
-	  }
-    return matches;	  
-	}
+  * A regular expression that matches any number of consecutive vowels
+  */
+  private static Pattern vowels = Pattern.compile("([aouåeiyäö]+)*", Pattern.CASE_INSENSITIVE);
+
+  /**
+  * @return the number of syllables in the word
+  */
+  public int count(String phrase) {
+    Matcher vowelMatcher = vowels.matcher(phrase);
+    int matches = 0;
+    while (vowelMatcher.find()) {
+      String match = vowelMatcher.group();
+      if (!(match.length() == 0)) {
+        System.out.println("Match: " + match);
+        matches++;
+      }
+    }
+    return matches;
+  }
 }
