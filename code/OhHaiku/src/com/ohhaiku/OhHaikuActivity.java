f57239cecf249206a026bdf3b4a6fc65db1a786d
Jesper Josefsson <jesper.josefsson@gmail.com>
2012-04-20 15:23:43 +0200

    Add rudimentary hello world

--- code/OhHaiku/src/com/ohhaiku/OhHaikuActivity.java
+++ code/OhHaiku/src/com/ohhaiku/OhHaikuActivity.java
@@ -2,12 +2,15 @@ package com.ohhaiku;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class OhHaikuActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        TextView tw = new TextView(this);
+        tw.setText("Hello world!");
+        setContentView(tw);
     }
 }
