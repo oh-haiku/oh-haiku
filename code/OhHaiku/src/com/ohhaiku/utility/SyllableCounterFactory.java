c9b52f22a3a9e1e1c4d64cc83537a8ba5658a86e
Jesper Josefsson <jesper.josefsson@gmail.com>
2012-04-29 23:43:07 +0200

    Implement boilerplate code for SyllableCounterFactory [#8]

--- code/OhHaiku/src/com/ohhaiku/utility/SyllableCounterFactory.java
+++ code/OhHaiku/src/com/ohhaiku/utility/SyllableCounterFactory.java
@@ -1,5 +1,20 @@
 package com.ohhaiku.utility;
 
+/**
+ * Extending classes are used to provide SyllableCounters of a certain language 
+ */
 public abstract class SyllableCounterFactory {
+  private String language;
+  
   public abstract SyllableCounter getSyllableCounter();
+  
+  /**
+   * @return The language of the Factory
+   */
+  public String getLanguage() {
+    return language;
+  }
+  public void setLanguage(String language) {
+    this.language = language;
+  }
 }
