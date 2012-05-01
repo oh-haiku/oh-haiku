f6867c8ac7def7d9e22273757a9ae202524eebf0
unknown <Victor@Victor-PC.(none)>
2012-05-01 14:33:51 +0200

    Added Activities

---
+++ code/OhHaiku/src/com/ohhaiku/TweetAHaikuActivity.java
@@ -0,0 +1,18 @@
+package com.ohhaiku;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+/*
+ * Activity that allows the user to tweet a Haiku.
+ */
+
+public class TweetAHaikuActivity extends Activity{
+	 @Override
+	 public void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        TextView tw = new TextView(this);
+	        tw.setText("Tweet a haiku");
+	        setContentView(tw);
+	 }
+}
