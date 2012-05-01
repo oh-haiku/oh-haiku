be68d813296383587eb7ff12af92cd697e8ac9a4
Jesper Josefsson <jesper.josefsson@gmail.com>
2012-05-01 19:07:04 +0200

    Tests are currently failing. Can't see why.

--- code/OhHaikuTest/src/com/ohhaiku/test/PoemTests.java
+++ code/OhHaikuTest/src/com/ohhaiku/test/PoemTests.java
@@ -5,9 +5,28 @@ import com.ohhaiku.models.Poem;
 import junit.framework.TestCase;
 
 public class PoemTests extends TestCase {
-  public void test() {
-    Poem p = new Poem();
+  
+  private Poem p;
+  
+  @Override
+  protected void setUp() {
+    p = new Poem();
+  }
+  
+  public void ItShouldReturnALine() {
     p.setLine(0, "Apa");
     assertEquals("Apa", p.getLine(0));
   }
+  
+  public void ItShouldNotReturnALine() {
+    p.setLine(0, "Apa");
+    assertEquals(null, p.getLine(1));
+  }
+  
+//  public void ItShouldReturnAPrettyString() {
+//    p.setLine(0, "Apa");
+//    p.setLine(1, "Bepa");
+//    p.setLine(2, "Cepa");
+//    assertEquals("Apa\nBepa\nCepa", p.toString());
+//  }
 }
