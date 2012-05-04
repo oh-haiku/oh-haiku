31ce2e0dab53a2737f9c0f67a47327e992fba8f0
Victor Karlsson <victorc@student.chalmers.se>
2012-05-04 10:31:14 +0200

    Created MenuActivity and updated AndroidManifest

--- code/OhHaiku/src/com/ohhaiku/BrowseSavedHaikusActivity.java
+++ code/OhHaiku/src/com/ohhaiku/BrowseSavedHaikusActivity.java
@@ -1,4 +1,5 @@
 package com.ohhaiku;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
--- code/OhHaiku/src/com/ohhaiku/HaikuCompositionActivity.java
+++ code/OhHaiku/src/com/ohhaiku/HaikuCompositionActivity.java
@@ -8,7 +8,7 @@ import android.widget.TextView;
  * Main activity: shows the Haiku composition window.
  */
 
-public class OhHaikuActivity extends Activity {
+public class HaikuCompositionActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
