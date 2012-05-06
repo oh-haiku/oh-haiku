f77909b11bd23dec7dd479f2d0019ce565da0bbf
Jesper Josefsson <jesper.josefsson@gmail.com>
2012-05-06 21:47:08 +0200

    Tweak db version to trigger table drop

--- code/OhHaiku/src/com/ohhaiku/database/DatabaseHelper.java
+++ code/OhHaiku/src/com/ohhaiku/database/DatabaseHelper.java
@@ -15,7 +15,7 @@ import com.ohhaiku.models.Poem;
 
 public class DatabaseHelper extends OrmLiteSqliteOpenHelper {
   private static final String DATABASE_NAME = "ohhaiku.db";
-  private static final int DATABASE_VERSION = 2;
+  private static final int DATABASE_VERSION = 1;
   
   private Dao<Poem, Integer> poemDao;
   
