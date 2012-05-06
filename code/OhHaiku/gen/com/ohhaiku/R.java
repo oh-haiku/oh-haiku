8157174d59b8e0707cb924032a425957fa7cd2d0
Jesper Josefsson <jesper.josefsson@gmail.com>
2012-05-06 22:34:15 +0200

    Changes to resources

--- code/OhHaiku/gen/com/ohhaiku/R.java
+++ code/OhHaiku/gen/com/ohhaiku/R.java
@@ -11,15 +11,35 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int bg2=0x7f020000;
+        public static final int bg_all=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int editText1=0x7f050000;
-        public static final int editText2=0x7f050001;
+        public static final int Button01=0x7f05000f;
+        public static final int Button02=0x7f05000e;
+        public static final int Button03=0x7f05000d;
+        public static final int CheckHaikuButton=0x7f050008;
+        public static final int MenuButton=0x7f05000a;
+        public static final int SaveButton=0x7f05000c;
+        public static final int TweetHaikuButton=0x7f050009;
+        public static final int button1=0x7f050010;
+        public static final int buttonSignIn=0x7f050004;
+        public static final int chkRememberPassword=0x7f050003;
+        public static final int editTextRow1=0x7f050005;
+        public static final int editTextRow2=0x7f050006;
+        public static final int editTextRow3=0x7f050007;
+        public static final int mediaController1=0x7f050000;
+        public static final int textView1=0x7f05000b;
+        public static final int txtPassword=0x7f050002;
+        public static final int txtUserName=0x7f050001;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
-        public static final int menu=0x7f030001;
+        public static final int browse=0x7f030000;
+        public static final int help=0x7f030001;
+        public static final int logintwitter=0x7f030002;
+        public static final int main=0x7f030003;
+        public static final int menu=0x7f030004;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
