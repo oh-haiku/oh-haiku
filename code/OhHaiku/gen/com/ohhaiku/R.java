4b0eb595142ca317d03bdd3c1c5637b2d44a21b9
Jesper Josefsson <jesper.josefsson@gmail.com>
2012-05-07 10:01:28 +0200

    Move button titles to resource [#9]

--- code/OhHaiku/gen/com/ohhaiku/R.java
+++ code/OhHaiku/gen/com/ohhaiku/R.java
@@ -44,7 +44,12 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040001;
+        public static final int certified_text=0x7f040007;
+        public static final int check_button_title=0x7f040006;
         public static final int hello=0x7f040000;
-        public static final int menuButton=0x7f040002;
+        public static final int help_button_title=0x7f040004;
+        public static final int menu_button_title=0x7f040005;
+        public static final int save_button_title=0x7f040003;
+        public static final int tweet_button_title=0x7f040002;
     }
 }
