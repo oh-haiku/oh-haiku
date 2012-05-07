5684e740ffbbdda205ae4aa2bce26d771bbb0ecd
Jesper Josefsson <jesper.josefsson@gmail.com>
2012-05-07 10:25:27 +0200

    Switch test project to API 8, correct tests and add tests Previous tests failed because of Android naming convention Seems that all tests must begin with "test"

--- code/OhHaikuTest/src/com/ohhaiku/test/PoemTests.java
+++ code/OhHaikuTest/src/com/ohhaiku/test/PoemTests.java
@@ -1,3 +1,7 @@
+/**
+ * @author Jesper Josefsson
+ */
+
 package com.ohhaiku.test;
 
 import com.ohhaiku.models.Poem;
@@ -6,27 +10,50 @@ import junit.framework.TestCase;
 
 public class PoemTests extends TestCase {
   
+  private static final String line0 = "apa";
+  private static final String line1 = "bepa";
+  private static final String line2 = "cepa";
+  
   private Poem p;
+  private Poem populatedPoem;
   
   @Override
   protected void setUp() {
     p = new Poem();
+    populatedPoem = new Poem(new String[] {line0, line1, line2});
   }
   
-  public void ItShouldReturnALine() {
+  public void testItShouldReturnALine() {
+    p = new Poem();
     p.setLine(0, "Apa");
     assertEquals("Apa", p.getLine(0));
   }
   
-  public void ItShouldNotReturnALine() {
+  public void testItShouldReturnAnEmptyLine() {
     p.setLine(0, "Apa");
-    assertEquals(null, p.getLine(1));
+    assertEquals("", p.getLine(1));
   }
   
-//  public void ItShouldReturnAPrettyString() {
-//    p.setLine(0, "Apa");
-//    p.setLine(1, "Bepa");
-//    p.setLine(2, "Cepa");
-//    assertEquals("Apa\nBepa\nCepa", p.toString());
-//  }
+  public void testItShouldReturnAPrettyString() {
+    p.setLine(0, "Apa");
+    p.setLine(1, "Bepa");
+    p.setLine(2, "Cepa");
+    assertEquals("Apa\nBepa\nCepa", p.toString());
+  }
+  
+  /*
+   * The line getters are used for persisting poems
+   * Not intended for actual use by the programmer
+   */
+  public void testLine0Getter() {
+    assertEquals(line0, populatedPoem.getLine0());
+  }
+  
+  public void testLine1Getter() {
+    assertEquals(line1, populatedPoem.getLine1());
+  }
+  
+  public void testLine2Getter() {
+    assertEquals(line2, populatedPoem.getLine2());
+  }
 }
