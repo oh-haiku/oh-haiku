7d50d1f2c2f4a483cbaa59247173d850ba82eff3
Jesper Josefsson <jesper.josefsson@gmail.com>
2012-05-07 10:56:15 +0200

    Add onClick handlers to MenuActivity [#14]

--- code/OhHaiku/src/com/ohhaiku/MenuActivity.java
+++ code/OhHaiku/src/com/ohhaiku/MenuActivity.java
@@ -1,7 +1,8 @@
 package com.ohhaiku;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
 
 /*
  * Activity that shows the main menu
@@ -11,8 +12,22 @@ public class MenuActivity extends Activity {
 	@Override
     public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-	    TextView tw = new TextView(this);
-	    tw.setText("Menu");
-	    setContentView(tw);
+		setContentView(R.layout.menu);
+	}
+	
+	public void goToSavedHaikus(View view) {
+	  startActivity(new Intent(this, BrowseSavedHaikusActivity.class));
+	}
+	
+	public void goToHelp(View view) {
+	  startActivity(new Intent(this, HelpActivity.class));
+	}
+	
+	public void goToFAQ(View view) {
+	  // TODO: write gotoFAQ method
+	}
+	
+	public void loginToTwitter(View view) {
+	  // TODO write login to twitter method
 	}
 }
