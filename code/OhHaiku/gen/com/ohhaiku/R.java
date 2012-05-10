2998d84d1f5d7ae259c1148ac9b7cba6b284bc4f
Jesper Gunnerling <jesper.gunnerling@gmail.com>
2012-05-10 19:48:18 +0200

    Working on XML [#9]

--- code/OhHaiku/gen/com/ohhaiku/R.java
+++ code/OhHaiku/gen/com/ohhaiku/R.java
@@ -16,25 +16,28 @@ public final class R {
         public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int Button01=0x7f050010;
-        public static final int Button02=0x7f05000f;
-        public static final int Button03=0x7f05000e;
-        public static final int CheckHaikuButton=0x7f050008;
-        public static final int MenuButton=0x7f05000a;
-        public static final int SaveButton=0x7f05000d;
-        public static final int TweetHaikuButton=0x7f050009;
-        public static final int button1=0x7f050011;
+        public static final int Button01=0x7f050013;
+        public static final int Button02=0x7f050012;
+        public static final int Button03=0x7f050011;
+        public static final int CheckHaikuButton=0x7f05000b;
+        public static final int MenuButton=0x7f05000d;
+        public static final int SaveButton=0x7f050010;
+        public static final int TweetHaikuButton=0x7f05000c;
+        public static final int button1=0x7f050014;
         public static final int buttonSignIn=0x7f050004;
+        public static final int checkbox1=0x7f050006;
+        public static final int checkbox2=0x7f050008;
+        public static final int checkbox3=0x7f05000a;
         public static final int chkRememberPassword=0x7f050003;
         public static final int editTextRow1=0x7f050005;
-        public static final int editTextRow2=0x7f050006;
-        public static final int editTextRow3=0x7f050007;
-        public static final int haikuText=0x7f050013;
-        public static final int haikuVerifiedCheckBox=0x7f050014;
-        public static final int icon=0x7f050012;
+        public static final int editTextRow2=0x7f050007;
+        public static final int editTextRow3=0x7f050009;
+        public static final int haikuText=0x7f050016;
+        public static final int haikuVerifiedCheckBox=0x7f050017;
+        public static final int icon=0x7f050015;
         public static final int listView=0x7f050000;
-        public static final int saveAndStatusView=0x7f05000b;
-        public static final int statusText=0x7f05000c;
+        public static final int saveAndStatusView=0x7f05000e;
+        public static final int statusText=0x7f05000f;
         public static final int txtPassword=0x7f050002;
         public static final int txtUserName=0x7f050001;
     }
