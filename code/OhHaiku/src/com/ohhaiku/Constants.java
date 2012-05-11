02663cbbdc0889271d13bf07000263b8a3ca1171
Jesper Josefsson <jesper.josefsson@gmail.com>
2012-05-11 11:13:38 +0200

    Move global constants to separate class

---
+++ code/OhHaiku/src/com/ohhaiku/Constants.java
@@ -0,0 +1,6 @@
+package com.ohhaiku;
+
+public class Constants {
+  public static final int LOAD_HAIKU = 123898914;
+  public static final String HAIKU_ID = Constants.class.getPackage().getName() + "HAIKU_ID";
+}
