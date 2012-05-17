619480e792fdc85f8556325c14ac36a98af2b707
Victor Karlsson <victorc@student.chalmers.se>
2012-05-17 16:40:11 +0200

    Minor change in strings

--- code/OhHaiku/src/com/ohhaiku/HaikuCompositionActivity.java
+++ code/OhHaiku/src/com/ohhaiku/HaikuCompositionActivity.java
@@ -290,7 +290,7 @@ public class HaikuCompositionActivity extends OrmLiteBaseActivity<DatabaseHelper
       }
     }
     else{
-      Toast.makeText(this, "Cannot save/update an empty Haiku!", Toast.LENGTH_LONG).show(); 
+      Toast.makeText(this, R.string.empty_rows_text, Toast.LENGTH_LONG).show(); 
     }
         
   }
