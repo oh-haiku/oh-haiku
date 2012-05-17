c0953c8581049419f51cca51e40809c2c41c279a
Jesper Josefsson <jesper.josefsson@gmail.com>
2012-05-17 10:12:10 +0200

    A first attempt at removing Haikus. List reload is non-functional [#29]

--- code/OhHaiku/src/com/ohhaiku/BrowseSavedHaikusActivity.java
+++ code/OhHaiku/src/com/ohhaiku/BrowseSavedHaikusActivity.java
@@ -4,11 +4,16 @@ import java.sql.SQLException;
 import java.util.List;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
@@ -25,6 +30,7 @@ import com.ohhaiku.views.PoemAdapter;
 public class BrowseSavedHaikusActivity extends OrmLiteBaseActivity<DatabaseHelper> {
 	private ListView lv;
 	private List<Poem> poems;
+  private ListAdapter adapter;
 	
   @Override
   public void onCreate(Bundle savedInstanceState) {
@@ -32,6 +38,10 @@ public class BrowseSavedHaikusActivity extends OrmLiteBaseActivity<DatabaseHelpe
     setContentView(R.layout.browse);
     lv = (ListView) findViewById(R.id.listView);
     fetchPoems();
+    setUpClickListeners();
+  }
+
+  private void setUpClickListeners() {
     lv.setOnItemClickListener(new OnItemClickListener() {
 
       public void onItemClick(AdapterView<?> parent, View view, int position, 
@@ -44,13 +54,52 @@ public class BrowseSavedHaikusActivity extends OrmLiteBaseActivity<DatabaseHelpe
       }
       
     });
+    
+    lv.setOnItemLongClickListener(new OnItemLongClickListener() {
+
+      public boolean onItemLongClick(AdapterView<?> parent, View view, int position,
+          long id) {
+        BrowseSavedHaikusActivity activity = BrowseSavedHaikusActivity.this;
+        final Poem p = (Poem) parent.getItemAtPosition(position);
+        Builder b = new Builder(activity);
+        b.setTitle("Delete a Haiku");
+        b.setMessage("Would you like to delete the following Haiku?\n" + p.toString());
+        b.setPositiveButton("Delete", new OnClickListener() {
+
+          public void onClick(DialogInterface dialog, int which) {
+            deletePoem(p);
+          }});
+        b.setNegativeButton("Cancel", new OnClickListener() {
+
+          public void onClick(DialogInterface dialog, int which) {
+            dialog.dismiss();
+          }
+          
+        });
+        AlertDialog ad = b.create();
+        ad.show();
+        return true;
+      }
+      
+    });
   }
 
+  private void deletePoem(Poem p) {
+    Dao<Poem, Integer> dao;
+    try {
+      dao = getHelper().getPoemDao();
+      dao.delete(p);
+    } catch (SQLException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+  }
+  
   private void fetchPoems() {
     try {
       Dao<Poem, Integer> poemDao = getHelper().getPoemDao();
       poems = poemDao.queryForAll();
-      ListAdapter adapter = new PoemAdapter(this, R.layout.poem, poems);
+      adapter = new PoemAdapter(this, R.layout.poem, poems);
       lv.setAdapter(adapter);
     } catch (SQLException e) {
       throw new RuntimeException("Could not get Haikus");
