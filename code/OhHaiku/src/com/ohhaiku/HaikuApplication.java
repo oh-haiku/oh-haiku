2f07b3078745a98891f162eebe787b1088e9b7b6
Linus Oleander <linus@oleander.nu>
2012-05-18 12:21:25 +0200

    Create Application class for storage

---
+++ code/OhHaiku/src/com/ohhaiku/HaikuApplication.java
@@ -0,0 +1,63 @@
+/**
+ * 
+ */
+package com.ohhaiku;
+
+
+import oauth.signpost.OAuthProvider;
+import oauth.signpost.commonshttp.CommonsHttpOAuthConsumer;
+import twitter4j.Twitter;
+import android.app.Application;
+
+public class HaikuApplication extends Application {
+	
+	
+	private Twitter twitter;
+	/**
+	 * @return the twitter
+	 */
+	public Twitter getTwitter() {
+		return twitter;
+	}
+
+	/**
+	 * @param twitter the twitter to set
+	 */
+	public void setTwitter(Twitter twitter) {
+		this.twitter = twitter;
+	}
+
+	private OAuthProvider provider;
+	private CommonsHttpOAuthConsumer consumer;
+	
+
+	/**
+	 * @param provider the provider to set
+	 */
+	public void setProvider(OAuthProvider provider) {
+		this.provider = provider;
+	}
+
+	/**
+	 * @return the provider
+	 */
+	public OAuthProvider getProvider() {
+		return provider;
+	}
+
+	/**
+	 * @param consumer the consumer to set
+	 */
+	public void setConsumer(CommonsHttpOAuthConsumer consumer) {
+		this.consumer = consumer;
+	}
+
+	/**
+	 * @return the consumer
+	 */
+	public CommonsHttpOAuthConsumer getConsumer() {
+		return consumer;
+	}
+
+
+}
